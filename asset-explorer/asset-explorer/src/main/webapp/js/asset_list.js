cec408d7e9ddffb8d65c28912b3131ac12444a47
Aurther.Wang <Aurther.Wang@gmail.com>
2014-05-14 14:31:23 +0000

    Committed 05/14/2014

--- asset-explorer/asset-explorer/src/main/webapp/js/asset_list.js
+++ asset-explorer/asset-explorer/src/main/webapp/js/asset_list.js
@@ -1,3 +1,28 @@
+function show_instore_dialog() {
+	$("div#_DIALOG_LAYER_2").hide();
+	$("div#_DIALOG_LAYER_1").show();
+}
+
+function show_inuse_dialog() {
+	$("div#_DIALOG_LAYER_1").hide();
+	$("div#_DIALOG_LAYER_2").show();
+}
+
+function changeStatus(status) {
+	if(status.options[status.selectedIndex].text == "In Store") {
+		show_instore_dialog();
+		status.options[1].selected = true;
+	} else if (status.options[status.selectedIndex].text == "In Use") {
+		show_inuse_dialog();
+		status.options[0].selected = true;
+	}
+}
+
+function closeDialog() {
+	$("div#_DIALOG_LAYER_1").hide();
+	$("div#_DIALOG_LAYER_2").hide();
+}
+
 $(document).ready(function() {
    $( "a#actions_btn" ).mouseover(function() {
         $("ul#action_menu").slideDown("fast");
@@ -9,4 +34,6 @@ $(document).ready(function() {
 	}).mouseenter(function() {
 	    clearTimeout(timer);
 	});
+   
+   $("a#changeStatus").click(show_instore_dialog);
 });
